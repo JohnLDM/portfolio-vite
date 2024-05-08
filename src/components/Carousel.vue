08fa797cbafb08a95f57d3bf51f4fa6f17f2043e
JohnQuimson <john.quimson777@gmail.com>
2024-05-08 22:44:44 +0100

    feat: setting up technology icons

---
+++ src/components/Carousel.vue
@@ -0,0 +1,45 @@
+<script>
+export default {
+  name: 'Carousel',
+  data() {
+    return {
+      technologies: [
+        'bootstrap.png',
+        'css.png',
+        'html.png',
+        'javascript.png',
+        'laravel.png',
+        'mysql.png',
+        'php.svg',
+        'sass.png',
+        'vite.png',
+        'vue.png',
+      ],
+    };
+  },
+};
+</script>
+
+<template>
+  <section>
+    <ul>
+      <li v-for="technology in technologies">
+        <div class="cont-img">
+          <img :src="`../../public/img/technologies/${technology}`" alt="img" />
+        </div>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<style scoped lang="scss">
+@use '../assets/scss/style.scss' as *;
+
+.cont-img {
+  width: 100px;
+
+  img {
+    width: 100%;
+  }
+}
+</style>
