1811415be727b9ca0ca087e86aa33560f1e19516
JohnQuimson <john.quimson777@gmail.com>
2024-05-14 14:45:43 +0100

    feat: about cards animation

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,19 +1,50 @@
 <script>
+import CardAbout from './CardAbout.vue';
+
 export default {
   name: 'About',
   data() {
-    return {};
+    return {
+      items: [
+        { text: "I'm currently living in the beautiful country of italy" },
+        {
+          text: "I'm fascinated by brilliant minds and ideas, and how there ideas can became reality",
+        },
+      ],
+    };
   },
-  components: {},
+  components: {
+    CardAbout,
+  },
 };
 </script>
 
 <template>
   <section id="about">
     <h2>About</h2>
-    <div class="about-container d-flex">
-      <div class="cont-pic border border-danger">
-        <img src="/public/img/profile-pic.jpg" alt="" />
+    <div class="about-container">
+      <div class="first-row d-flex justify-content-center">
+        <!-- picture -->
+        <div class="cont-pic">
+          <img src="/public/img/profile-pic.jpg" alt="" />
+        </div>
+
+        <!-- riga 1 col 2 -->
+        <div class="custom-card">
+          <p>
+            I'm a Jr Full stack web developer, always passionate about
+            technology and what's behind it
+          </p>
+        </div>
+      </div>
+
+      <div class="first-row d-flex justify-content-center">
+        <CardAbout
+          class=""
+          v-for="(item, index) in items"
+          :key="index"
+          :text="item.text"
+        />
       </div>
     </div>
   </section>
@@ -22,9 +53,29 @@ export default {
 <style lang="scss">
 @use '../assets/scss/style.scss' as *;
 
-@keyframes shift {
+#about {
+  background-color: var(--custom-primary);
+}
+
+h2 {
+  color: var(--custom-white);
+  font-size: clamp(70px, 10vw, 200px);
+  line-height: clamp(70px, 10vw, 200px);
+  font-weight: 800;
+}
+
+@keyframes shiftR {
   0% {
-    transform: translateX(-100%);
+    transform: translateX(-350%);
+  }
+  40% {
+    transform: translateX(0);
+  }
+}
+
+@keyframes shiftL {
+  0% {
+    transform: translateX(300%);
   }
   40% {
     transform: translateX(0);
@@ -32,8 +83,8 @@ export default {
 }
 
 .cont-pic {
-  margin-left: clamp(30px, 10vw, 300px);
-  animation: shift linear;
+  // margin-left: clamp(30px, 10vw, 300px);
+  animation: shiftR linear;
   animation-timeline: view();
   height: clamp(150px, 30vw, 10000px);
   width: clamp(150px, 30vw, 10000px);
@@ -42,10 +93,46 @@ export default {
   align-items: center;
   border-radius: 10%;
   overflow: hidden;
+  margin: clamp(20px, 5vw, 50px);
 
   img {
     height: 100%;
     object-fit: cover;
   }
+}
+
+.custom-card {
+  // margin-right: clamp(30px, 10vw, 300px);
+  animation: shiftL linear;
+  animation-timeline: view();
+  height: clamp(150px, 30vw, 10000px);
+  width: clamp(150px, 30vw, 10000px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10%;
+  overflow: hidden;
+  background-color: var(--custom-primary);
+  border: 2px solid var(--custom-white);
+  color: var(--custom-white);
+  margin: clamp(20px, 5vw, 50px);
+
+  img {
+    height: 100%;
+    object-fit: cover;
+  }
+
+  p {
+    text-transform: uppercase;
+    font-size: clamp(10px, 2vw, 300px);
+    text-align: center;
+    padding: 0 clamp(20px, 3vw, 50px);
+    font-weight: 600;
+  }
+}
+
+.cont-text {
+  animation: shiftL linear;
+  animation-timeline: view();
 }
 </style>
---
+++ src/components/CardAbout.vue
@@ -0,0 +1,61 @@
+<script>
+export default {
+  name: 'CardAbout',
+  data() {
+    return {};
+  },
+  components: {},
+
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="custom-card2">
+    <p>{{ text }}</p>
+  </div>
+</template>
+
+<style lang="scss">
+@use '../assets/scss/style.scss' as *;
+
+@keyframes fade {
+  0% {
+    opacity: 0;
+  }
+  30% {
+    opacity: 0;
+  }
+  40% {
+    opacity: 1;
+  }
+}
+
+.custom-card2 {
+  animation: fade linear;
+  animation-timeline: view();
+  height: clamp(150px, 30vw, 10000px);
+  width: clamp(150px, 30vw, 10000px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10%;
+  background-color: var(--custom-primary);
+  border: 2px solid var(--custom-white);
+  color: var(--custom-white);
+  margin: clamp(20px, 5vw, 50px);
+
+  p {
+    text-transform: uppercase;
+    font-size: clamp(10px, 2vw, 300px);
+    text-align: center;
+    padding: 0 clamp(20px, 3vw, 50px);
+    font-weight: 600;
+  }
+}
+</style>
